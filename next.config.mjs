cc83241fb11cf2cdc00f8bddde52da642319f0e9
Julio <juliohn@gmail.com>
2024-09-15 23:01:24 +1000

    vercel
    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-// next.config.mjs
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
-    return config;
-  },
   images: {
     domains: ["preodemo.gumlet.io"],
   },
